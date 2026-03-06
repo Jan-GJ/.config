d66fd9353a8b0f1f7f28f981f16ad9547928dfdc
Jan-GJ <janjuettermann@gmail.com>
2026-03-06 15:59:00 +0100

    fix: working oxlint and oxfmt

--- nvim/init.lua
+++ nvim/init.lua
@@ -71,6 +71,7 @@ local plugins = {
 	{ src = "https://github.com/folke/snacks.nvim" },
 	{ src = "https://github.com/Saghen/blink.cmp" },
 	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/soulsam480/nvim-oxlint" }
 
 	-- { src = "https://github.com/zbirenbaum/copilot.lua" },
 	-- { src = "https://github.com/copilotlsp-nvim/copilot-lsp" }
@@ -430,6 +431,134 @@ require "blink.cmp".setup {
 -- 	}
 -- })
 --
+--
+local util = require 'lspconfig.util'
+vim.lsp.config("oxfmt",
+
+	{
+		cmd = function(dispatchers, config)
+			local cmd = 'oxfmt'
+			local local_cmd = (config or {}).root_dir and config.root_dir .. '/node_modules/.bin/oxfmt'
+			if local_cmd and vim.fn.executable(local_cmd) == 1 then
+				cmd = local_cmd
+			end
+			return vim.lsp.rpc.start({ cmd, '--lsp' }, dispatchers)
+		end,
+		filetypes = {
+			'javascript',
+			'javascriptreact',
+			'typescript',
+			'typescriptreact',
+			'toml',
+			'json',
+			'jsonc',
+			'json5',
+			'yaml',
+			'html',
+			'vue',
+			'handlebars',
+			'css',
+			'scss',
+			'less',
+			'graphql',
+			'markdown',
+		},
+		workspace_required = true,
+		root_dir = function(bufnr, on_dir)
+			local fname = vim.api.nvim_buf_get_name(bufnr)
+
+			-- Oxfmt resolves configuration by walking upward and using the nearest config file
+			-- to the file being processed. We therefore compute the root directory by locating
+			-- the closest `.oxfmtrc.json` / `.oxfmtrc.jsonc` (or `package.json` fallback) above the buffer.
+			local root_markers = util.insert_package_json({ '.oxfmtrc.json', '.oxfmtrc.jsonc' }, 'oxfmt', fname)
+			on_dir(vim.fs.dirname(vim.fs.find(root_markers, { path = fname, upward = true })[1]))
+		end,
+	}
+
+)
+
+local function oxlint_conf_mentions_typescript(root_dir)
+	local fn = vim.fs.joinpath(root_dir, '.oxlintrc.json')
+	for line in io.lines(fn) do
+		if line:find('typescript') then
+			return true
+		end
+	end
+	return false
+end
+
+
+
+vim.lsp.config("oxlint",
+	{
+		cmd = function(dispatchers, config)
+			local cmd = 'oxlint'
+			local local_cmd = (config or {}).root_dir and config.root_dir .. '/node_modules/.bin/oxlint'
+			if local_cmd and vim.fn.executable(local_cmd) == 1 then
+				cmd = local_cmd
+			end
+			return vim.lsp.rpc.start({ cmd, '--lsp' }, dispatchers)
+		end,
+		filetypes = {
+			'javascript',
+			'javascriptreact',
+			'typescript',
+			'typescriptreact',
+			'vue',
+			'svelte',
+			'astro',
+		},
+		root_markers = { '.oxlintrc.json', 'oxlint.config.ts' },
+		workspace_required = true,
+
+		on_attach = function(client, bufnr)
+			vim.api.nvim_buf_create_user_command(bufnr, 'LspOxlintFixAll', function()
+				client:exec_cmd({
+					title = 'Apply Oxlint automatic fixes',
+					command = 'oxc.fixAll',
+					arguments = { { uri = vim.uri_from_bufnr(bufnr) } },
+				})
+			end, {
+				desc = 'Apply Oxlint automatic fixes',
+			})
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				buffer = bufnr,
+				callback = function()
+					client:exec_cmd({
+						title = 'Apply Oxlint automatic fixes',
+						command = 'oxc.fixAll',
+						arguments = { { uri = vim.uri_from_bufnr(bufnr) } },
+					}, { bufnr = bufnr })
+				end,
+			})
+		end,
+
+
+		settings = {
+			-- run = 'onType',
+			-- configPath = nil,
+			-- tsConfigPath = nil,
+			-- unusedDisableDirectives = 'allow',
+			-- typeAware = false,
+			-- disableNestedConfig = false,
+			-- fixKind = 'safe_fix',
+		},
+		before_init = function(init_params, config)
+			local settings = config.settings or {}
+			if settings.typeAware == nil and vim.fn.executable('tsgolint') == 1 then
+				local ok, res = pcall(oxlint_conf_mentions_typescript, config.root_dir)
+				if ok and res then
+					settings = vim.tbl_extend('force', settings, { typeAware = true })
+				end
+			end
+			local init_options = config.init_options or {}
+			init_options.settings = vim.tbl_extend('force', init_options.settings or {} --[[@as table]], settings)
+
+			init_params.initializationOptions = init_options
+		end,
+	}
+)
+
 
 vim.filetype.add({
 	extension = {
